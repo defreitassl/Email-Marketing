bc8115d939a4ba51f6deb627612a892701da48b0
Douglas Freitas <douglasamf14@gmail.com>
2024-10-06 02:35:42 -0300

    demo register-client route

--- app/routes/clients.py
+++ app/routes/clients.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 client = Blueprint('client', __name__)
 
@@ -6,3 +6,13 @@ client = Blueprint('client', __name__)
 @client.route('/clients')
 def clients_route():
     return render_template('clients.html')
+
+
+@client.route('/register', methods=['POST',])
+def register_client():
+
+
+    nome = request.form['name']
+    email = request.form['email']
+
+    return f"<p>Nome: {nome}</p><p>Email: {email}</p>"
